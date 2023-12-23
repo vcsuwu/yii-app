817308cc21a3975d0783a12217f435f6e2a939e8
vcsuwu <zenyabtw@gmail.com>
2023-12-23 23:58:02 +0500

    initial commit

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=myapp',
+    'username' => 'vocus',
+    'password' => '1234',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
